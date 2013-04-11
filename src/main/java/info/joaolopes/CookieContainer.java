6934eba4e85d3063c73f4f05dbbfd3c8f11afac7
DPr00f <mail@joaolopes.info>
2013-04-11 23:15:39 +0100

    Finished HttpWrapper

--- src/main/java/info/joaolopes/CookieContainer.java
+++ src/main/java/info/joaolopes/CookieContainer.java
@@ -1,13 +1,214 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package info.joaolopes;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
 
 /**
  *
- * @author Pink Donut <joaolopes at pinkdonut.net>
+ * @author João Lopes <mail at joaolopes.info>
  */
 public class CookieContainer {
+    private class CookieInfo {
+        public String domain;
+        public String path;
+        public Date expires;
+        public Boolean secure;
+        public String name;
+        public String value;
+        
+        public CookieInfo(){
+            this.domain = "";
+            this.secure = false;
+            this.path = "/";
+            Calendar cal = Calendar.getInstance();
+            cal.set(Calendar.YEAR, cal.get(Calendar.YEAR) + 10);
+
+            this.expires = cal.getTime();
+        }
+    }
+    
+    
+    
+    
+    private List<CookieInfo> cookiesInfo;
+    
+    public CookieContainer(){
+        cookiesInfo = new ArrayList<>();
+    }
+    
+    public void addCookie(String cookieString){
+        addCookie(cookieString, "");
+    }
+    
+    public void addCookie(String cookieString, String domain){
+        CookieInfo ci = new CookieInfo();
+        ci.domain = domain;
+        String[] cookieData = cookieString.split("; ");
+        
+        for (String cookieD : cookieData) {
+            String[] info = cookieD.split("=", 2);
+            if(info[0].equalsIgnoreCase("path")){
+                ci.path = info[1];
+            }else if( info[0].equalsIgnoreCase("httponly")){
+                ci.secure = true;
+            }else if( info[0].equalsIgnoreCase("domain")){
+                ci.domain = info[1];
+            }else if (info[0].equalsIgnoreCase("expires")){
+                SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd-MMM-yyyy HH:mm:ss z", Locale.ENGLISH);
+                try {
+                    ci.expires = sdf.parse(info[1]);
+                } catch (ParseException ex) {}
+            }else{
+                ci.name = info[0];
+                ci.value = info[1];
+            }
+        }
+        
+        if(!hasCookie(ci.name, ci.domain)){
+            cookiesInfo.add(ci);
+        }else{
+            cookiesInfo.set(getCookieNumber(ci.name, ci.domain), ci);
+        }
+        
+        
+    }
+    
+    private int getCookieNumber(String key, String domain){
+        int i = 0;
+        for (CookieInfo cookieInfo : cookiesInfo) {
+            if(cookieInfo.name.equals(key)){
+                return i;
+            }
+            i++;
+        }
+        
+        return -1;
+    }
+    
+    @Override
+    public String toString(){
+        String cookies = "";
+        for (CookieInfo cookieInfo : cookiesInfo) {
+            cookies = cookies.concat(cookiesString(cookieInfo.domain));
+        }
+        return cookies;
+    }
+    private String cookiesString(String domain){
+        String cookies = "";
+        SimpleDateFormat df = new SimpleDateFormat("EEE, dd-MMM-yyyy HH:mm:ss z", Locale.ENGLISH);
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+        for (CookieInfo cookieInfo : cookiesInfo) {
+            if(cookieInfo.domain.equals(domain)){
+                cookies += cookieInfo.name + "=" + cookieInfo.value;
+                cookies += "; expires=" + df.format(cookieInfo.expires);
+                cookies += "; path=" + cookieInfo.path;
+                if(domain.length() > 0){
+                    cookies += "; domain=" + cookieInfo.domain;
+                }
+                if(cookieInfo.secure){
+                    cookies += "; httponly";
+                }
+                cookies += "\n";
+            }
+        }
+        
+        return cookies;
+    }
+    public boolean hasCookie(String name) { return hasCookie(name, ""); }
+    public boolean hasCookie(String name, String domain){
+        for (CookieInfo cookieInfo : cookiesInfo) {
+            if(cookieInfo.domain.equals(domain)){
+                if(cookieInfo.name.equals(name)) return true;
+            }
+        }
+        return false;
+    }
+    
+    public String getHeaderCookies(String domain){
+        if(isEmpty()) return "";
+        String cookies = "";
+        for (CookieInfo cookieInfo : cookiesInfo) {
+            if(new Date().before(cookieInfo.expires)){
+                cookies += cookieInfo.name + "=" + cookieInfo.value + "; ";
+            }
+        }
+        cookies = cookies.substring(0, cookies.length() - 2);
+        
+        return cookies;
+    }
+    
+    public void clear(){
+        this.cookiesInfo.clear();
+    }
+    
+    public boolean isEmpty(){
+        return cookiesInfo.isEmpty();
+    }
+    
+    public List<String> keys(){
+        return keys("");
+    }
+    public List<String> keys(String domain){
+        List<String> toReturn = new ArrayList<>();
+        for (CookieInfo cookieInfo : cookiesInfo) {
+            if(cookieInfo.domain.equals(domain)){
+                toReturn.add(cookieInfo.name);
+            }
+        }
+        return toReturn;
+    }
+    
+    public String read(String key){
+        return read(key, null);
+    }
+    
+    public String read(String key, String domain){
+        for (CookieInfo cookieInfo : cookiesInfo) {
+            if(domain != null){
+                if(cookieInfo.domain.equals(domain)){
+                    if(cookieInfo.name.equals(key)){
+                        return cookieInfo.value;
+                    }
+                }
+            }else{
+                String val = this.read(key, cookieInfo.domain);
+                if(val != null){
+                    return val;
+                }
+            }
+        }
+        return null;
+    }
+    
+    
+    public void remove(String key){
+        remove(key, null);
+    }
+    
+    public void remove(String key, String domain){
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd-MMM-yyyy HH:mm:ss z", Locale.ENGLISH);
+        for (CookieInfo cookieInfo : cookiesInfo) {
+            if(domain != null){
+                if(cookieInfo.domain.equals(domain)){
+                    if(cookieInfo.name.equals(key)){
+                        try {
+                            cookieInfo.expires = sdf.parse("Thu, 01 Jan 1970 00:00:00 GMT");
+                        } catch (ParseException ex) {}
+                    }
+                }
+            }else{
+                this.remove(key, cookieInfo.domain);
+            }
+        }
+    }
+    
+   
     
 }
--- src/main/java/info/joaolopes/HttpWrapper.java
+++ src/main/java/info/joaolopes/HttpWrapper.java
@@ -1,32 +1,164 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package info.joaolopes;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  *
- * @author Pink Donut <joaolopes at pinkdonut.net>
+ * @author João Lopes <mail at joaolopes.info>
  */
 public class HttpWrapper {
+    String url;
     public static enum Action{
-        GET, POST
+        GET, POST, PUT, DELETE
     }
     
     public static enum Type{
         Normal, Json, Ajax, Json_Ajax
     }
     
-    public String fetch(String url, String data, Type type, Action action) throws MalformedURLException, IOException{
+    public CookieContainer cookies;
+    private String Json;
+    private Type requestType;
+    private Map<String, String> parameters;
+    private Action requestAction;
+    private String host;
+    private String referer;
+    
+    public HttpWrapper(){
+        cookies = new CookieContainer();
+        requestType = Type.Normal;
+        parameters = new HashMap<>();
+        requestAction = Action.GET;
+        Json = "{}";
+        host = "";
+        referer = "";
+    }
+    
+    public void setReferer(String referer){
+        this.referer = referer;
+    }
+    
+    public String getReferer(){
+        return referer;
+    }
+    
+    public void setHost(String host){
+        this.host = host;
+    }
+    
+    public String getHost(){
+        return host;
+    }
+    
+    public void addParameter(String name, String value){
+        parameters.put(name, value);
+    }
+    
+    public void removeParameter(String name){
+        parameters.remove(name);
+    }
+    
+    public void clearParameters(){
+        parameters.clear();
+    }
+    
+    public void setType(Type requestType){
+        this.requestType = requestType;
+    }
+    
+    public Type getType(){
+        return requestType;
+    }
+    
+    public void setJson(String Json){
+        this.Json = Json;
+    }
+    
+    public String getJson(){
+        return Json;
+    }
+    
+    public void setAction(Action requestAction){
+        this.requestAction = requestAction;
+    }
+    
+    public Action getAction(){
+        return requestAction;
+    }
+    
+    public String request() throws UnsupportedEncodingException, MalformedURLException, IOException{
+        if(requestType == Type.Json || requestType == Type.Json_Ajax){
+            String _json = Json;
+            Json = "{}";
+            
+            return HttpWrapper.request(url, _json, requestType, requestAction, host, referer, cookies);
+        }
+        String params = getParametersString();
+        clearParameters();
+        
+        return HttpWrapper.request(url, params, requestType, requestAction, host, referer, cookies);
+    }
+    
+    private String getParametersString() throws UnsupportedEncodingException{
+        String params = "";
+        Iterator it = parameters.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry entry = (Map.Entry) it.next();
+            params = params.concat(URLEncoder.encode((String)entry.getKey(), "UTF-8") + "=" + URLEncoder.encode((String)entry.getValue(), "UTF-8"));
+            it.remove();
+            if(it.hasNext()) params = params.concat("&");
+        }
+        return params;
+    }
+    
+    public void clearCookies(){
+        cookies.clear();
+    }
+    
+    public void setUrl(String url) throws MalformedURLException{
+        this.url = url;
+    }
+    
+    public void setUrl(URL url){
+        this.url = url.toString();
+    }
+    
+    public String getUrl(){
+        return this.url;
+    }
+    
+    
+    public static String request(String url) throws MalformedURLException, IOException{
+        return request(url, "");
+    }
+    public static String request(String url,  String data) throws MalformedURLException, IOException{
+        return request(url, data, Type.Normal);
+    }
+    public static String request(String url,  String data, Type type) throws MalformedURLException, IOException{
+        return request(url, data, type, Action.GET);
+    }
+    public static String request(String url,  String data, Type type, Action action) throws MalformedURLException, IOException{
+        return request(url, data, type, action, "");
+    }
+    public static String request(String url,  String data, Type type, Action action, String host) throws MalformedURLException, IOException{
+        return request(url, data, type, action, host, "");
+    }
+    public static String request(String url,  String data, Type type, Action action, String host, String referer) throws MalformedURLException, IOException{
+        return request(url, data, type, action, host, referer, null);
+    }
+    public static String request(String url, String data, Type type, Action action, String host, String referer, CookieContainer cookieContainer) throws MalformedURLException, IOException{
         String resultLines = "";
         URL _url;
         URLConnection urlConnection;
@@ -34,14 +166,14 @@ public class HttpWrapper {
         BufferedReader inStream;
         String body = data;
         
-        
         // Create connection
-        if (action == Action.GET) {
+        if (action == Action.GET || action == Action.DELETE) {
             if(body.length() > 0){
                 url += "?" + body;
             }
         }
         _url = new URL(url);
+        
         urlConnection = _url.openConnection();
         if(action == Action.GET){
             ((HttpURLConnection)urlConnection).setRequestMethod("GET");
@@ -51,13 +183,30 @@ public class HttpWrapper {
             ((HttpURLConnection)urlConnection).setRequestMethod("POST");
             urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
         }
+        if(action == Action.PUT){
+            ((HttpURLConnection)urlConnection).setRequestMethod("PUT");
+            urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        }
+        if(action == Action.DELETE){
+            ((HttpURLConnection)urlConnection).setRequestMethod("DELETE");
+            urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        }
         System.setProperty("http.agent", "");
         urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.100 Safari/534.30");
+        if(!host.isEmpty()){
+            urlConnection.setRequestProperty("Host", host);
+        }
+        
+        if(!referer.isEmpty()){
+            urlConnection.setRequestProperty("Referer", referer);
+        }
+        
         urlConnection.setDoInput(true);
         urlConnection.setUseCaches(false);
         
-        //conn.setRequestProperty("Host", "steamcommunity.com");
-        //conn.setRequestProperty("Referer", "http://steamcommunity.com/trade/1");
+        if(cookieContainer != null){
+            urlConnection.setRequestProperty("Cookie", cookieContainer.getHeaderCookies(_url.getHost()));
+        }
         
         if(type == Type.Json || type == Type.Json_Ajax){
             urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
@@ -67,7 +216,7 @@ public class HttpWrapper {
             urlConnection.setRequestProperty("X-Prototype-Version", "1.7");
         }
         
-        if(action == Action.POST){
+        if(action == Action.POST || action == Action.PUT){
             urlConnection.setDoOutput(true);
             urlConnection.setRequestProperty("Content-Length", ""+ body.length());
             outStream = new OutputStreamWriter(urlConnection.getOutputStream());
@@ -81,19 +230,22 @@ public class HttpWrapper {
         while((buffer = inStream.readLine()) != null) {
             resultLines = resultLines.concat(buffer + "\n");
         }
-        
-        
        
         inStream.close();
         if(outStream != null){
             outStream.close();
         }
         
-        String headerName=null;
-        for (int i=1; (headerName = urlConnection.getHeaderFieldKey(i))!=null; i++) {
-            if (headerName.equals("Set-Cookie")) {                  
-                String cookie = urlConnection.getHeaderField(i);
-                System.out.println(cookie);
+        if(cookieContainer != null){
+            String headerName;
+            for (int i=1; (headerName = urlConnection.getHeaderFieldKey(i))!=null; i++) {
+                if (headerName.equals("Set-Cookie")) {                  
+                    String cookie = urlConnection.getHeaderField(i);
+                    if(!cookie.contains("domain=")){
+                        cookie = cookie.concat("; domain=" + _url.getHost());
+                    }
+                    cookieContainer.addCookie(cookie);
+                }
             }
         }
         
